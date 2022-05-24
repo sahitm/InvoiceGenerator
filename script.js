52a0160d6950265b164bd89ef7bc4d794783376e
sahitm1 <67734756+sahitm@users.noreply.github.com>
2022-05-24 15:27:29 +0000

    changed code so it would remove only one element

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-let newElements = document.querySelector("#invoiceText")
-console.log(newElements)
+let CWText = document.querySelector('.CW')
+CWText.style.display = "none";
+
+let MLText = document.querySelector('.ML')
+MLText.style.display = "none";
+
+let PWText = document.querySelector('.PW')
+PWText.style.display = "none";
 
 let amount = document.querySelector("#amount")
 let incrementAmount = 0
 
-function addButtonElements(h1,price,BtnId){
-  newElements.innerHTML += 
-  ` <div id="invoiceTextinside">
-     <div id="taskText">
-        <h1 id="taskTextH1">${h1}</h1>
-        <p  id="taskTextP"><button id="removeBtn" onclick="removeBtn()" >remove</button></p>
-      </div>
-      <div  id="taskTextPrice">$${price}</div>
-   </div>
-  `
-  incrementAmount +=  Number(price)
-  amount.innerHTML = '$' + incrementAmount
-  document.getElementById(BtnId).disabled = true;
+function btnClick(hiddenText,BtnId,price){
+    let Htext = document.querySelector(hiddenText)
+    Htext.style.display = "grid";
+
+    incrementAmount +=  Number(price)
+    amount.innerHTML = '$' + incrementAmount
+
+    document.getElementById(BtnId).disabled = true;
 }
 
-function removeBtn(){
-  document.getElementById("invoiceText").innerHTML=""
-  document.getElementById("washCarBtn").disabled = false;
-  document.getElementById("mowLawnBtn").disabled = false;
-  document.getElementById("pullWeedsBtn").disabled = false;
-  amount.innerHTML = "$0"
-  incrementAmount = 0
-}
+function removeBtn(hiddenText,BtnRmId,rmprice){
+    let removeID = document.querySelector(hiddenText)
+    removeID.style.display = "none";
 
+    incrementAmount -=  Number(rmprice)
+    amount.innerHTML = '$' + incrementAmount
 
-
+    document.getElementById(BtnRmId).disabled = false;
+  }
